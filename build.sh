8e26364b0a4e03322413ae249da2c969cb3f91f2
Jonathan Nicholas <Jonathan.Nicholas@protonmail.com>
2019-02-11 22:12:24 +1100

    build script

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+cd static/img/_original 
+find . -maxdepth 1 -iname "*.jpg" | xargs -L1 -I{} convert -resize 50% "{}" "{}.rsz"
+mv *.rsz ../
+cd ..
+rename -v 's/.rsz//' *
+cd ../..
+hugo
+cd public
+surge .
