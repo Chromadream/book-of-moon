394e2b10d55d25ec9fa1ca9adf9051c6e8d3326d
Jonathan Nicholas <Jonathan.Nicholas@protonmail.com>
2019-03-03 17:07:59 +1100

    header logo fix

---
+++ nogit.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+cd static/img/_original 
+find . -maxdepth 1 -iname "*.jpg" | xargs -L1 -I{} convert -resize 50% "{}" "{}.rsz"
+mv *.rsz ../
+cd ..
+rename -v 's/.rsz//' *
+rm -f *.rsz
+cd ../..
+hugo
+cd public
+surge .
